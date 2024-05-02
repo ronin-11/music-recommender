e387d8e4336a3ac068d68d703cc12b71840b320e
ronin-11 <94402374+ronin-11@users.noreply.github.com>
2024-05-02 22:04:04 +0200

    Update music.py

--- music.py
+++ music.py
@@ -34,7 +34,7 @@ df_tmp = pd.DataFrame(similarities, columns=df["Song Name"], index=df["Song Name
 
 # Streamlit app
 def main():
-    st.title("Song Recommendation System")
+    st.title("Music Recommendation System")
 
     st.write("Enter the name of a song in lower_case to get recommendations:")
 
